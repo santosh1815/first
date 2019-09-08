8b6b56866d3a40474c12faa85aa397b2dcee5f37
Santosh <s.chilakawad@gmail.com>
2019-09-08 13:44:01 +0530

    Removed instance

--- createResources.tf
+++ createResources.tf
@@ -9,12 +9,6 @@ resource "aws_instance" "i1" {
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "i2" {
-  ami           = "ami-0c322300a1dd5dc79"
-  instance_type = "t2.micro"
-}
-
-
 resource "aws_eip" "eip1" {
   instance = "${aws_instance.i1.id}"
 }
