8fd93b53e80177df4df8d885af80fc8171c99d28
Santosh <s.chilakawad@gmail.com>
2019-09-08 13:40:49 +0530

    Missed the quotes, fixed it

--- createResources.tf
+++ createResources.tf
@@ -3,39 +3,12 @@ provider "aws" {
   secret_key = "rXKFfs7b+033Svp57ytO4tdoE1rrESa1E9ktX0yU"
   region     = "us-east-1"
 }
-
-resource "aws_instance" "i1" {
+resource "aws_instance" "example1" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "i2" {
-  ami           = "ami-0c322300a1dd5dc79"
+resource "aws_instance" "example2" {
+  ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
-}
-
-
-resource "aws_eip" "eip1" {
-  instance = "${aws_instance.i1.id}"
-}
-
-
-resource "aws_s3_bucket" "b1" {
-  bucket = "tecmax0908201901"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "b2" {
-  bucket = "tecmax0908201902"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
