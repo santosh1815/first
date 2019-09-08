eb17e5cb4db16f758013a3e149253c9b4b790034
Santosh <s.chilakawad@gmail.com>
2019-09-08 13:41:56 +0530

    Changes

--- createResources.tf
+++ createResources.tf
@@ -1,14 +1,41 @@
 provider "aws" {
-  access_key = AKIA4HB3VCA4GGU3K62K"
+  access_key = "AKIA4HB3VCA4GGU3K62K"
   secret_key = "rXKFfs7b+033Svp57ytO4tdoE1rrESa1E9ktX0yU"
   region     = "us-east-1"
 }
-resource "aws_instance" "example1" {
+
+resource "aws_instance" "i1" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "example2" {
-  ami           = "ami-0b69ea66ff7391e80"
+resource "aws_instance" "i2" {
+  ami           = "ami-0c322300a1dd5dc79"
   instance_type = "t2.micro"
+}
+
+
+resource "aws_eip" "eip1" {
+  instance = "${aws_instance.i1.id}"
+}
+
+
+resource "aws_s3_bucket" "b1" {
+  bucket = "tecmax0908201901"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "b2" {
+  bucket = "tecmax0908201902"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
